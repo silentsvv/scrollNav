967aa695982a47e91ffb13d5a5ad8108eb6389f0
wu_ze_qing <wuzeqing@zuzuche.com>
2018-05-11 18:43:36 +0800

    add: 添加demo

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
 
-
 ;(function (name, definition) {
-    console.log('123')
     // 检测上下文环境是否为AMD或CMD
     var hasDefine = typeof define === 'function',
     // 检查上下文环境是否为Node
@@ -18,22 +16,50 @@
         this[name] = definition();
     }
 })('NavScroll',function () {
-        function NavScroll(option) {
-            let self = this;
+
+        /**
+         * 初始化滚动插件
+         * 
+         * @param {any} option {
+         *      navQuery(string): '[data-nav]', //querySelectorAll
+         *      posQuery(string): '[data-pos]', //querySelectorAll
+         *      navNode(node元素): '默认搜索[data-nav]的元素',
+         *      posNode(node元素): '默认搜索[data-pos]的元素',
+         *      clickFn(function): '点击菜单回调事件',
+         *      initError(function): '初始化失败事件',
+         *      taggleClassName: 'active' //菜单激活按钮类名,
+         *      
+         * }
+         */
+        function NavScroll(option = {}) {
+            let self         = this;
 
             self.activeIndex = 0; //当前滚动位置(对应的激活菜单)
-            self.tmpIndex = 0; //滚动时对应的位置(对应的激活菜单)
-            self.navNode = option?option.navNode:document.querySelectorAll('[data-nav]'); 
-            self.posNode = option?option.posNode:document.querySelectorAll('[data-pos]');
-            console.log(self.navNode);
-            self.posArr = [];
-            console.log('init');
-            self._init();
+            self.tmpIndex    = 0; //滚动时对应的位置(对应的激活菜单)
+            self.navNode     = option.navNode?option.navNode: document.querySelectorAll(option.navQuery?option.navQuery: '[data-nav]'); //菜单按钮node节点数组
+            self.posNode     = option.posNode?option.posNode: document.querySelectorAll(option.posQuery?option.posQuery: '[data-pos]'); //位置元素按钮数组
+            self.posArr      = []; //位置记录数组
+            self.option      = option; //保存option
+
+            self.option.taggleClassName = self.option.taggleClassName?self.option.taggleClassName:'active'; //初始化
+
+            try {
+                self._init();
+            } catch (error) {
+                console.log(error);
+                return option.initError?option.initError():false;
+            }
+            
         }
 
         NavScroll.prototype = {
             constructor: this,
         
+            /**
+             * 初始化事件
+             * 
+             * @returns 
+             */
             _init() {
                 let self = this;
                 if(self.posNode.length != self.navNode.length) {
@@ -46,6 +72,10 @@
                 self._initScrollEvent();
             },
         
+            /**
+             * 获取定位元素高度数组
+             * 
+             */
             _initPosArr() {
                 let self = this;
         
@@ -55,29 +85,38 @@
                 })
             },
         
+            /**
+             * 初始化菜单按钮点击事件
+             * 
+             */
             _initNavClick() {
                 let self = this;
         
                 self.navNode.forEach((item, index) => {
                     item.onclick = () => {
-                        window.scrollTo(0, self.posArr[index])
+                        window.scrollTo(0, self.posArr[index]);
+                        //判断有没有传进事件
+                        return self.option.clickFn?self.option.clickFn():false;
                     }
                 })
             },
         
+            /**
+             * 滚动事件绑定
+             * 
+             */
             _initScrollEvent() {
                 let self = this;
         
                 window.onscroll = () => {
                     let arrIndex = 0;
-                    console.log('src')
+
                     while(document.body.scrollTop?document.body.scrollTop:document.documentElement.scrollTop >= self.posArr[arrIndex]) {
                         arrIndex++;
                         self.tmpIndex = arrIndex - 1;
                     }
         
                     if(self.tmpIndex != self.activeIndex) {
-                        console.log('change');
                         self.activeIndex = self.tmpIndex;
         
                         self.navNode.forEach((item, index) => {
@@ -90,6 +129,12 @@
                 }
             },
         
+            /**
+             * 获取距顶高度
+             * 
+             * @param {any} element 
+             * @returns 
+             */
             _getElePosition(element) {
                 var topPosition = 0;
         
